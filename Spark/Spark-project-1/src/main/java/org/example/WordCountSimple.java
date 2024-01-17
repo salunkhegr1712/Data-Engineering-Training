9066ec9f1f1290f59f1c028dda21b3df846fef60
salunkhegr1712 <salunkhegr1712@gmail.com>
2024-01-17 22:21:47 +0530

    completed day-2 code for spark

--- Spark/Spark-project-1/src/main/java/org/example/WordCountSimple.java
+++ Spark/Spark-project-1/src/main/java/org/example/WordCountSimple.java
@@ -3,11 +3,16 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import scala.Int;
 import scala.Tuple2;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class WordCountSimple {
+    static int getWordCount(String string,String word){
+        return  1;
+    }
+
     public static void main(String[] args) {
 
         String url = "file:///C:\\Trainings\\file.txt";
@@ -27,13 +32,43 @@ public class WordCountSimple {
 
         JavaSparkContext sparkContext = new JavaSparkContext(spark.sparkContext());
         JavaRDD<String> lines = sparkContext.textFile(url);
-        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator())
-                .filter(word -> !word.equals("to"));
-        //remove any word that is an article a, an, to, in, the, as, is
-        JavaPairRDD<String,Integer> pairRDD = words.mapToPair(w -> new Tuple2<>(w, 1));
-        JavaPairRDD<String, Integer> counts = pairRDD.reduceByKey((x, y) ->  x + y);
+        System.out.println(lines);
 
-        counts.collect().forEach(System.out::println);
+
+        List<String> listOfLines= lines.collect();
+        List<String> allWords=new LinkedList<>();
+
+        for (int i = 0; i < listOfLines.size(); i++) {
+//            System.out.println("line "+(i+1)+" : "+listOfLines.get(i));
+            List<String>buff=new ArrayList<>(Arrays.asList(listOfLines.get(i).split(" ")));
+            System.out.println("words are : "+buff);
+            allWords.addAll(buff);
+        }
+
+        Map<String, Integer> map= new TreeMap<String,Integer>();
+        for (int i = 0; i < allWords.size(); i++) {
+            String word=allWords.get(i);
+            if(map.containsKey(word)){
+                map.replace(word,map.get(word)+1);
+            }
+            else {
+                map.put(word,1);
+            }
+        }
+
+        List<String> keys=new ArrayList<>(map.keySet());
+        List<Integer> values= new ArrayList<>(map.values());
+
+        for (int i = 0; i < keys.size(); i++) {
+            System.out.println("( "+keys.get(i)+" : "+values.get(i)+" )");
+        }
+//        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator())
+//                .filter(word -> !word.equals("to"));
+//        //remove any word that is an article a, an, to, in, the, as, is
+//        JavaPairRDD<String,Integer> pairRDD = words.mapToPair(w -> new Tuple2<>(w, 1));
+//        JavaPairRDD<String, Integer> counts = pairRDD.reduceByKey((x, y) ->  x + y);
+
+//        counts.collect().forEach(System.out::println);
         //counts.saveAsTextFile("path to file");
 
         try {
